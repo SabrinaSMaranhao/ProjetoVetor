2b7bd07c361b3abe7576c3c19c245d6b037aab0f
Sabrina <sabrina.maranhao@etec.sp.gov.br>
2025-04-22 22:10:13 -0300

    Primeiro commit

---
+++ Clientes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoVetor
+{
+    internal class Clientes
+    {
+        public string Nome { get; set; }
+        public bool ClientesPrioritarios { get; set; }
+        public string Email;
+        protected string CPF;
+        private string Senha;
+        Clientes[] user = new Clientes[10];
+
+
+        public void cadastrar()
+        {
+           
+            Console.WriteLine(" Digite seu nome:");
+            this.Nome = Console.ReadLine();
+
+            Console.WriteLine(" Digite seu Email:");
+            this.Email = Console.ReadLine();
+
+            Console.WriteLine(" Digite seu CPF:");
+            this.CPF = Console.ReadLine();
+
+            Console.WriteLine(" Digite uma senha:");
+            this.Senha = Console.ReadLine();
+        }
+        
+        
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoVetor
+{
+    internal class Program:Clientes
+    {
+        static void Main(string[] args)
+
+
+
+        {
+            Console.WriteLine("Nome do Cliente:");
+            string Nome = Console.ReadLine();
+
+            Console.WriteLine("É prioritario?");
+            string entrada = Console.ReadLine();
+
+        }
+    }
+}
